da099e21f7ab4cdd4ae0628fc7d684f083731140
Igoc <zx0342@gmail.com>
2022-04-20 16:36:03 +0900

    Fix: Update mongo config

--- conf/mongo.py
+++ conf/mongo.py
@@ -2,7 +2,8 @@ import os
 
 config = {
     'host': os.getenv('MONGODB_HOST', 'localhost'),
-    'port': int(os.getenv('MONGODB_PORT', '27017'))
+    'port': int(os.getenv('MONGODB_PORT', '27017')),
+    'db': 'stepwithdog'
 }
 
 if os.getenv('MONGODB_USER') != None and os.getenv('MONGODB_PASSWORD') != None:
