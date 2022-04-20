48496548ca09beebd854c047dee15bb34222b8eb
Igoc <zx0342@gmail.com>
2022-04-20 20:29:08 +0900

    Feat: Create image upload for saving post api

--- api/post_post.py
+++ api/post_post.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, current_app, jsonify, request
 
+from utils.image import convert_img_file_to_ndarray, convert_ndarray_to_base64
+
+import cv2
+
 bp = Blueprint('post', __name__, url_prefix='/post')
 
 
@@ -11,10 +15,22 @@ def save_post():
     content = data['content']
     address = data['address']
 
+    image = request.files['image']
+    image = convert_img_file_to_ndarray(image)
+    thumbnail = cv2.resize(image, dsize=(640, 480), interpolation=cv2.INTER_AREA)
+
+    _, image = cv2.imencode('.png', image)
+    _, thumbnail = cv2.imencode('.png', thumbnail)
+
+    image = 'data:image/png;base64,' + convert_ndarray_to_base64(image)
+    thumbnail = 'data:image/png;base64,' + convert_ndarray_to_base64(thumbnail)
+
     doc = {
         'title': title,
         'content': content,
-        'address': address
+        'address': address,
+        'image': image,
+        'thumbnail': thumbnail
     }
 
     current_app.db.posts.insert_one(doc)
