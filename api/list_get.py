de8b53566146a6efc1427bc4365593d567ac95ed
songunnie <kariber21@naver.com>
2022-04-21 19:57:29 +0900

    Fix: Update getting post list api

--- api/list_get.py
+++ api/list_get.py
@@ -7,7 +7,11 @@ bp = Blueprint('get_list', __name__, url_prefix='/post-list')
 @bp.route('', methods=['GET'])
 def get_list():
     # 크롤링한 데이터를 DB에서 가져온다
-    post_list = list(current_app.db.posts.find({}, {'_id': False, 'address': False, 'content': False, 'image': False}))
+    post_list = list(
+        current_app.db.posts.find({}, {'address': False, 'content': False, 'image': False}).sort('_id', -1))
+
+    for i in range(len(post_list)):
+        post_list[i]['_id'] = str(post_list[i]['_id'])
 
     # 가져온 데이터를 클라이언트에 보내준다
     return jsonify({'all_list': post_list})
